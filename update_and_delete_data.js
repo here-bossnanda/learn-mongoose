bdc01ccab1a04a1582c27f1e0146610bdcce89dd
here-bossnanda <m.trinandanoviardy@gmail.com>
2020-12-29 16:14:06 +0700

    [nanda] add update and delete

---
+++ update_and_delete_data.js
@@ -0,0 +1,22 @@
+const mongoose = require('./config/connection');
+const Fruit = require('./models/model');
+
+// UPDATE
+// UPDATE USING UPDATE ONE
+Fruit.updateOne({ _id: '5feadeee82ca9f52e8045edc' }, { name: 'Manggo', ranting: 4, review: 'rasa manggo' }, (err, fruit) => {
+    if (err) console.log(err);
+    else {
+        console.log(fruit, "success update apple to manggo");
+        mongoose.connection.close();
+    }
+})
+
+// DELETE 
+// DELETE USING DELETE ONE
+Fruit.deleteOne({ _id: '5feadeee82ca9f52e8045edf' }, (err, fruit) => {
+    if (err) console.log(err);
+    else {
+        console.log(fruit, "successfully delete kiwi");
+        mongoose.connection.close();
+    }
+})
